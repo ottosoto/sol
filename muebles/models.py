7c4ff881951eba7b4c38d038a23bb0b480970e8a
ottosoto <ottosoto@gmail.com>
2017-11-08 04:16:47 -0600

    Modificacion de pagina de inicio, y base, ademas se arreglaron los Urls, se copio la imagen de logo, y se creo la base de datos

--- muebles/models.py
+++ muebles/models.py
@@ -2,3 +2,37 @@ from django.db import models
 from django.utils import timezone
 from django.contrib import admin
 # Create your models here.
+#ACTOR      = Producto
+#PELICULA   = Compras
+#ACTUACION  = Detalle
+class Producto(models.Model):
+    id_categoria = models.CharField(max_length=30)
+    id_presentacion_madera = models.CharField(max_length=30)
+    nombre = models.CharField(max_length=30)
+    cantidad = models.CharField(max_length=6)
+    precio = models.CharField(max_length=30)
+    descripcion=models.CharField(max_length=60)
+    def __str__(self):
+        return self.nombre
+
+class Compra(models.Model):
+    fecha = models.DateField()
+    usuario = models.CharField(max_length=60)
+    productos = models.ManyToManyField(Producto, through='Detalle')
+    def __str__(self):
+        return self.usuario
+
+class Detalle (models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    compra = models.ForeignKey(Compra, on_delete=models.CASCADE)
+
+
+class DetalleInLine(admin.TabularInline):
+    model = Detalle
+    extra = 1
+
+class MaterialAdmin(admin.ModelAdmin):
+    inlines = (DetalleInLine,)
+
+class EncabezadoAdmin (admin.ModelAdmin):
+    inlines = (DetalleInLine,)
--- muebles/views.py
+++ muebles/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
+from django.contrib import messages
+#from PROYECTO_APP.forms import EncabezadoForm, MaterialForm
+#from PROYECTO_APP.models import Encabezado, Material, Descripcion
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+
+def index(request):
+    return render(request, 'muebles/index.html')
